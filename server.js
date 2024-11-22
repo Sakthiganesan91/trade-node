e24c73315498928b1e740bcd5674e8f480128992
Sakthiganesan91 <sakthiganesan158@gmail.com>
2024-11-22 16:24:56 +0530

    added web scoket

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 require("dotenv").config();
 
 const userAuthRoutes = require("./routes/user.routes");
-const webHookRoutes = require("./routes/webhook.routes");
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(cors({}));
 app.use(express.json());
 
 app.use(userAuthRoutes);
-app.use(webHookRoutes);
 
 app.get("/", async (req, res) => {
   res.send("hello");
@@ -21,7 +29,23 @@ app.get("/", async (req, res) => {
 const port = process.env.PORT;
 
 const mongodb_uri = process.env.MONGODB_URI;
+
+io.on("connection", (socket) => {
+  console.log("Connection successful");
+});
+
+app.post("/trade", async (req, res) => {
+  const data = req.body;
+
+  console.log(req);
+  console.log(data);
+
+  io.emit("tradingview-data", data);
+
+  res.json({});
+});
+
 mongoose.connect(mongodb_uri).then(() => {
-  app.listen(port);
+  httpServer.listen(port);
   console.log("Listening at port " + port);
 });
